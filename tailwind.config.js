5c916cf0a3623f798d94bc42bd270f8f1c51508c
mathiyalagan <mathiyalaganbrave@gmail.com>
2025-01-21 20:11:21 +0530

    Fix build configuration
    Changed PostCSS config to CommonJS syntax
    
    Updated tailwind config with custom colors
    
    Fixed module export syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/lib/esm/**/*.js"
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {
